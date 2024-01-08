f5191be23b31299675a8ad5d65bdc5a72555b147
Pasiphique <pasiphiqueoswald@gmail.com>
2024-01-08 07:02:21 -0500

    Hello world

---
+++ Furniture/src/App.jsx
@@ -0,0 +1,19 @@
+import NavBar from './Components/NavBar'
+import Hero from './Components/Hero'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
+import './App.css'
+function App() {
+ 
+
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
