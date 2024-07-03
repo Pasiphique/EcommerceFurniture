9720878c86cf3cfd667f6c1401d3fc29b75df829
Pasiphique <pasiphiqueoswald@gmail.com>
2024-07-03 14:33:24 -0400

    more stlying to footer section

--- Furniture/src/Footer.jsx
+++ Furniture/src/Footer.jsx
@@ -2,30 +2,30 @@ import {Link} from 'react-router-dom'
 export default function Footer(){
     return(
         <footer>
+            <h1>Funiro.</h1> 
             <div className='footer'>
-               <h1>Funiro</h1> 
-               <div className='link-container'>
-                    <ul className='links store'>
+               
+               <div className='link-container'> 
+                    <span>
                         <Link to='/'>Home</Link>
                         <Link to='/shop'>Shop</Link>
+                    </span>               
+                    <span>
                         <Link to='/contact'>Contact</Link>
-                    </ul>
-               </div>
-               <div className='link-container'>
-                    <ul className='links help'>
                         <Link to='/contact'>Payment Options</Link>
+                    </span>
+                    
+                    <span>
                         <Link to='/contact'>Returns</Link>
-                        <Link to='/contact'>Privacy Policies</Link>
-                        <Link to='/contact'>Terms of Use</Link>
-                    </ul>
+                        <Link to='/contact'>Privacy Policies</Link>  
+                    </span>
+                    <Link to='/contact'>Terms of Use</Link>  
+                                     
                </div>
-               <div className='link-container'>
-                    <form action="">
-                        <label htmlFor="">Enter Your Email Address</label>
-                        <input type="button" value={"SUBSCRIBE"} />
-                    </form>
-               </div>
-            </div>
+               <p>	&copy; 2024 Pasiphique Osward</p>
+        </div>
+        
         </footer>
+        
     )
 }
